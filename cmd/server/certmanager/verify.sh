2fef7468b20a3005faf78cf82f83cc16b71cf9ce
Desislav Ivanov <34714579+desoivanov@users.noreply.github.com>
2019-06-04 14:43:10 +0300

    cert manager

--- cmd/server/certmanager/verify.sh
+++ cmd/server/certmanager/verify.sh
@@ -2,13 +2,13 @@
 openssl verify -verbose -CAfile certs/CA/CA.pem certs/CA/CA.pem
 (openssl x509 -pubkey -in certs/CA/CA.pem -noout | openssl md5 ;\
    openssl pkey -pubout -in certs/CA/CA.key | openssl md5) | uniq
-openssl verify -verbose -CAfile certs/CA/CA.pem certs/SERVER/Server.pem
-(openssl x509 -pubkey -in certs/SERVER/Server.pem -noout | openssl md5 ;\
-   openssl pkey -pubout -in certs/SERVER/Server.key | openssl md5) | uniq
-openssl verify -verbose -CAfile certs/CA/CA.pem certs/CLIENT/Client.pem
-(openssl x509 -pubkey -in certs/CLIENT/Client.pem -noout | openssl md5 ;\
-   openssl pkey -pubout -in certs/CLIENT/Client.key| openssl md5) | uniq
+openssl verify -verbose -CAfile certs/CA/CA.pem certs/SERVER/default/Server.pem
+(openssl x509 -pubkey -in certs/SERVER/default/Server.pem -noout | openssl md5 ;\
+   openssl pkey -pubout -in certs/SERVER/default/Server.key | openssl md5) | uniq
+openssl verify -verbose -CAfile certs/CA/CA.pem certs/CLIENT/default/Client.pem
+(openssl x509 -pubkey -in certs/CLIENT/default/Client.pem -noout | openssl md5 ;\
+   openssl pkey -pubout -in certs/CLIENT/default/Client.key| openssl md5) | uniq
 
 openssl x509 -in certs/CA/CA.pem -text -noout
-openssl x509 -in certs/SERVER/Server.pem -text -noout
-openssl x509 -in certs/CLIENT/Client.pem -text -noout
+openssl x509 -in certs/SERVER/default/Server.pem -text -noout
+openssl x509 -in certs/CLIENT/default/Client.pem -text -noout
